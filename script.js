ed00f0776b4d712bca9e39097061ac4404490171
sebamrios <sebastian.m.rios@gmail.com>
2023-04-06 16:23:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ end.value = "";
 
 let today = new Date();
 let dd = today.getDate();
-let ddPlus1 = today.getDate()+1
+let ddPlus1 = today.getDate() + 1
 let mm = today.getMonth() + 1; //January is 0!
 let yyyy = today.getFullYear();
 if (dd < 10) {
-  dd = '0' + dd
+    dd = '0' + dd
 }
 if (mm < 10) {
-  mm = '0' + mm
+    mm = '0' + mm
 }
 
 today = yyyy + '-' + mm + '-' + dd;
@@ -25,7 +25,7 @@ start.min = today;
 end.min = today;
 
 function validar() {
-    if(start.value>=end.value){
+    if (start.value >= end.value) {
         alert("La fecha de salida debe ser posterior a la entrada")
     }
 }
